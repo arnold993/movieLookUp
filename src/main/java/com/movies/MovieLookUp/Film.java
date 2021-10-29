6559f4759eeee9d2498f43c2a47a3e5d9228748f
Arnold Quarcoo <aquarcoo@softwareinstitute.com>
2021-10-29 15:34:22 +0100

    update check

--- src/main/java/com/movies/MovieLookUp/Film.java
+++ src/main/java/com/movies/MovieLookUp/Film.java
@@ -3,8 +3,36 @@ package com.movies.MovieLookUp;
 public class Film {
     private String title;
     private String description;
-    private String release_year;
+    private int release_year;
     private String language;
     private int duration;
-    private String rating;
+    private int rating;
+
+    public Film (String title, String description, int release_year, String language, int duration, int rating){
+        this.title = title;
+        this.description = description;
+        this.release_year = release_year;
+        this.language = language;
+        this.duration = duration;
+        this.rating = rating;
+    }
+
+    public String getTitle() { return title;}
+    public void  setTitle(String title) {this.title = title;}
+
+    public String getDescription() { return description;}
+    public void  setDescription(String description) {this.description = description;}
+
+    public int getRelease_year() { return release_year;}
+    public void  setRelease_year(int release_year) {this.release_year = release_year;}
+
+    public String getLanguage() { return language;}
+    public void  setLanguage(String language) {this.language = language;}
+
+    public int getDuration() { return duration;}
+    public void  setDuration(int duration) {this.duration = duration;}
+
+    public int getRating() { return rating;}
+    public void  setRating(int rating) {this.rating = rating;}
+
 }
