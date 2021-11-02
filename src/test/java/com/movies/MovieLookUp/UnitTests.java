5ea931c9b3c820ad7229a7dcf4fec83f38b9ec2d
Arnold Quarcoo <aquarcoo@softwareinstitute.com>
2021-11-02 13:59:10 +0000

    removed single unit tests and merged into 1 class

---
+++ src/test/java/com/movies/MovieLookUp/UnitTests.java
@@ -0,0 +1,37 @@
+package com.movies.MovieLookUp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnitTests {
+    @Test
+    public void testMovieTitle(){
+        Film titleOfMovie = new Film ( "Ironman, Age of Ultron","Ironman fights robots",2017,"English",188,8);
+        assertEquals("Movie title not found", "Ironman, Age of Ultron", titleOfMovie.getTitle());
+    }
+
+    @Test
+    public void testYearOfRelease() {
+        Film yearOfRelease = new Film("Ironman, Age of Ultron", "Ironman fights robots", 2017, "English", 188, 8);
+        assertEquals("Try another year", 2017, yearOfRelease.getRelease_year());
+    }
+
+    @Test
+    public void durationOfMovie() {
+        Film duration = new Film("Ironman, Age of Ultron", "Ironman fights robots", 2017, "English", 188, 8);
+        assertEquals("Movie with that duration not found", 188, duration.getDuration());
+    }
+
+    @Test
+    public void ratingOfMovie() {
+        Film rating = new Film("Ironman, Age of Ultron", "Ironman fights robots", 2017, "English", 188, 8);
+        assertEquals("Movie with that duration not found", 8, rating.getRating());
+    }
+
+    @Test
+    public void testRating() {
+        Film rating = new Film("Ironman, Age of Ultron", "Ironman fights robots", 2017, "English", 188, 8);
+        assertEquals("movie with that rating not found", 8, rating.getRating());
+    }
+}
