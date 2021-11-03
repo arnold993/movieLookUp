928b11169739a64877ccd6aa2468c2778ad1cbcd
Arnold Quarcoo <aquarcoo@softwareinstitute.com>
2021-11-03 19:46:58 +0000

    Unit tests name change

--- src/test/java/com/movies/MovieLookUp/UnitTests.java
+++ src/test/java/com/movies/MovieLookUp/UnitTests.java
@@ -1,49 +1,48 @@
 package com.movies.MovieLookUp;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.assertEquals;
-
-public class UnitTests {
+public class UnitTest {
     @Test
     public void testMovieTitle(){
         Film titleOfMovie = new Film (1001, "Ironman, Age of Ultron","Ironman fights robots",2017,1,188,"PG");
-        assertEquals("Movie title not found", "Ironman, Age of Ultron", titleOfMovie.getTitle());
+        assertEquals( "Ironman, Age of Ultron", titleOfMovie.getTitle(), "Title not correct");
     }
 
     @Test
     public void testYearOfRelease() {
         Film yearOfRelease = new Film(1001, "Ironman, Age of Ultron", "Ironman fights robots", 2017, 1, 188, "PG");
-        assertEquals("Try another year", 2017, yearOfRelease.getRelease_year());
+        assertEquals(2017, yearOfRelease.getRelease_year(), "Choose another year");
     }
 
     @Test
     public void durationOfMovie() {
         Film duration = new Film(1001, "Ironman, Age of Ultron", "Ironman fights robots", 2017, 1, 188, "PG");
-        assertEquals("Movie with that duration not found", 188, duration.getDuration());
+        assertEquals(188, duration.getDuration(), "Duration too short");
     }
 
     @Test
     public void ratingOfMovie() {
         Film rating = new Film(1001, "Ironman, Age of Ultron", "Ironman fights robots", 2017, 1, 188, "PG");
-        assertEquals("Movie with that duration not found", "PG", rating.getRating());
+        assertEquals( "PG", rating.getRating(), "Wrong rating");
     }
 
     @Test
     public void testFilmId() {
         Film filmId = new Film(1001, "Ironman, Age of Ultron", "Ironman fights robots", 2017, 1, 188, "PG");
-        assertEquals("Please insert valid ID", 1001, filmId.getFilm_id());
+        assertEquals( 1001, filmId.getFilm_id(),"Wrong ID");
     }
 
     @Test
     public void testLanguageId() {
         Film languageId = new Film(1001, "Ironman, Age of Ultron", "Ironman fights robots", 2017, 1, 188, "PG");
-        assertEquals("Please insert valid language ID", 1, languageId.getLanguage());
+        assertEquals(1, languageId.getLanguage(), "Choose right language");
     }
 
     @Test
     public void testDescription() {
         Film description = new Film(1001, "Ironman, Age of Ultron", "Ironman fights robots", 2017, 1, 188, "PG");
-        assertEquals("Description is not correct", "Ironman fights robots", description.getDescription());
+        assertEquals( "Ironman fights robots", description.getDescription(), "Wrong description");
     }
 }
