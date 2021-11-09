75b8db355206f06ed64cedc14a87fff6c24bb355
Arnold Quarcoo <aquarcoo@softwareinstitute.com>
2021-11-09 16:35:11 +0000

    AWS update

---
+++ src/main/java/com/movies/movielookup/AppConfig.java
@@ -0,0 +1,73 @@
+package com.movies.movielookup;
+
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.activation.DataSource;
+import java.util.Base64;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+
+    @Bean
+    public DataSource dataSource (){
+
+        AwsSecrets secrets=getSecret();
+        return DataSourceBuilder
+                .create()
+//                .driverClassName("com.mysql")
+                .url("")
+                .username(secrets.getUsername())
+                .password(secrets.getPassword())
+                .build();
+    }
+
+    private Gson gson = new Gson();
+
+
+    private AwsSecrets getSecret() {
+
+        String secretName = "databaseAWS";
+        String region = "us-east-2";
+
+
+        AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials((new AWSStaticCredentialsProvider((new BasicAWSCredentials(accessKey,secretKey)))))
+                .build();
+
+        String secret, decodedBinarySecret;
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                .withSecretId(secretName);
+        GetSecretValueResult getSecretValueResult = null;
+
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+
+            throw e;
+        }
+        if (getSecretValueResult.getSecretString() != null) {
+            secret = getSecretValueResult.getSecretString();
+
+            return gson.fromJson(secret, AwsSecrets.class);
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/com/movies/movielookup/AwsSecrets.java
@@ -0,0 +1,11 @@
+package com.movies.movielookup;
+
+public class AwsSecrets {
+    private String username;
+    private String password;
+    private String host;
+    private String engine;
+    private String port;
+    private String dbInstanceIdentifier;
+
+}
